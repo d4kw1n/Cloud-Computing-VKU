38a8018dfd99f17f2378402d233059ed5f8c2a1d
shr3wd <tripm2112@gmail.com>
2025-01-07 21:58:58 +0700

    Init server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+
+dotenv.config();
+
+app.use(express.static('./uploads'));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
