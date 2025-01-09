aa45dd460b23ea94f3d3aeb3b74e2ed37da42b95
shr3wd <tripm2112@gmail.com>
2025-01-09 18:45:06 +0700

    Add test template

--- index.js
+++ index.js
@@ -6,20 +6,30 @@ const path = require('path');
 const connectDB = require("./db/connect");
 
 const app = express();
-
+app.set('view engine', 'pug');
+app.set('views', './templates');
 dotenv.config();
 
-connectDB();
+// connectDB();
 
 app.use(express.static('./uploads'));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.static('public'));
 
 app.use("/uploads", express.static(path.join(__dirname, 'uploads')));
 app.use("/v1", routes);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+app.get('/template', (req, res) => {
+    res.render('index', { title: 'Trang Chủ', message: 'Chào mừng bạn đến với Pug!' });
+});
+
+app.get('/table', (req, res) => {
+    res.render('table', { title: 'Danh sách sản phẩm', message: 'Danh sách sản phẩm' });
 });
 
 app.listen(process.env.PORT || 3000, () => {
